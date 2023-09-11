b9b50cbb7d9acb9dd422645c524a74b2a283e228
GuiGebaili78 <103890957+GuiGebaili78@users.noreply.github.com>
2023-09-11 19:26:50 -0300

    primeiro commit finalizado

---
+++ Fiap.Web.AspNet/Repository/Context/DataBaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Fiap.Web.AspNet.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Fiap.Web.AspNet.Repository.Context
+{
+    public class DataBaseContext : DbContext
+    {
+
+        // Propriedade que será responsável pelo acesso a tabela de Representantes
+        public DbSet<RepresentanteModel> Representante { get; set; }
+
+        // Propriedade que será responsável pelo acesso a tabela de Cliente
+        public DbSet<ClienteModel> Cliente { get; set; }
+
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DataBaseContext()
+        {
+        }
+
+    }
+}
---
+++ Fiap.Web.AspNet/Repository/Context/RepresentanteRepository.cs
@@ -0,0 +1,66 @@
+﻿using Fiap.Web.AspNet.Models;
+
+namespace Fiap.Web.AspNet.Repository.Context
+{
+    public class RepresentanteRepository
+    {
+
+        private readonly DataBaseContext dataBaseContext;
+
+        public RepresentanteRepository(DataBaseContext ctx)
+        {
+            dataBaseContext = ctx;
+        }
+
+
+
+        public IList<RepresentanteModel> Listar()
+        {
+            var lista = new List<RepresentanteModel>();
+
+            // Efetuando a listagem (Substituindo o Select *)
+            lista = dataBaseContext.Representante.ToList();
+
+            return lista;
+        }
+
+        public RepresentanteModel Consultar(int id)
+        {
+            // Recuperando o objeto Representante de um determinado Id
+            var representante = dataBaseContext.Representante.Find(id);
+
+            return representante;
+        }
+
+        public void Inserir(RepresentanteModel representante)
+        {
+            // Adiciona o objeto preenchido pelo usuário
+            dataBaseContext.Representante.Add(representante);
+
+            // Salva as alterações
+            dataBaseContext.SaveChanges();
+
+        }
+
+        public void Alterar(RepresentanteModel representante)
+        {
+            dataBaseContext.Representante.Update(representante);
+
+            // Salva as alterações
+            dataBaseContext.SaveChanges();
+        }
+
+        public void Excluir(int id)
+        {
+            var representante = new RepresentanteModel(id, "");
+
+            dataBaseContext.Representante.Remove(representante);
+
+            // Salva as alterações
+            dataBaseContext.SaveChanges();
+
+        }
+
+
+    }
+}
